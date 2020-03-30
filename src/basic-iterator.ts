9e996c743bbfbe216915b29849465cc7436b0ce7
Bin Le <bin.le.code@gmail.com>
2020-03-30 11:48:00 -0400

    refine iterator example

--- src/basic-iterator.ts
+++ src/basic-iterator.ts
@@ -1,11 +1,11 @@
 
-
+// a mechanical custom iterator interface
 interface MyIterator {
     next(): any;
     hasNext(): boolean;
     reset(): void;
 }
-
+// a mechanical custom iterable interface
 interface MyIterable {
     iterator(): MyIterator;
 }
@@ -19,9 +19,9 @@ class MyIterableImpl implements MyIterable {
         return this.myIterator;
     }
 
-    /** 
+    /**
      * iterator property defined as an inner class implementing the Iterator interface
-     */ 
+     */
     myIterator: MyIterator = new class {
         // inner class constructor has input argument of container class
         constructor(public superThis: MyIterableImpl) {
@@ -58,8 +58,67 @@ while (myIter.hasNext()) {
     console.log(myIter.next());
 }
 
+/**
+ * A (common) practice of implementing a custom iterable class is implement
+ * [Symbol.iterator] interface that returns an iterator object that has:
+ * - next() method
+ * - return() method [optional], support early termination like 'break'
+ * this iterator filters out all non-number and negative numbers from interation
+ */
+class CustomIterable {
+    private data: Set<any>;
 
-// now use javascript stock iterator interface [Symbol.iterator] to support for-of loop
+    constructor(data: Array<any> | Set<any>) {
+        this.data = new Set(data);
+    }
+
+    
+    [Symbol.iterator]() {
+        let lst = Array.from(this.data);
+        let sz = lst.length;
+        console.log('length: ' + sz);
+        let idx = 0;
+        return {
+            next: () => {
+                let n = lst[idx++];
+                while (n) {
+                    console.log('... idx: ' + idx + ', typeof n: ' + (typeof n) + ', value of n: ' + n);
+                    if ((typeof n) == 'number' && n > 0) {
+                        break;
+                    }
+                    n = lst[idx++];
+                }
+
+                if (n) {
+                    return { done: false, value: n };
+                } else {
+                    return { done: true };
+                }
+            },
+            return() {
+                console.log('... break early')
+                return { done: true };
+            }
+        };
+    }
+}
+
+let ci = new CustomIterable([3, 'abc', -12, 11, 34, -5, 11, true, 28, -4, new Date(), 7]);
+// loop through the whole set
+for (let i of ci) {
+    console.log('positive number: ' + i);
+}
+// early termination
+for (let i of ci) {
+    if (i >= 10) {
+        console.log('stop iteration on getting first >= 10 nubmer: ' + i);
+        break;
+    }
+    console.log('gettting <10 number: ' + i);
+}
+
+
+// now implement *[Symbol.iterator] as generator
 
 class MyBag {
     items: any[] = [];
@@ -70,7 +129,6 @@ class MyBag {
         }
     }
 
-    // implement iterator generator function interface that for-of loop can call
     *[Symbol.iterator]() {
         let max = this.items.length;
         let idx = 0;
@@ -84,5 +142,3 @@ let mb = new MyBag(['apple', 'pen', 'laptop', 'usb-drive']);
 for (let item of mb) {
     console.log('bag has item: ' + item);
 }
-
-
