88e5886884050702ae1d8c116127413c35d9b48c
Bin Le <bin.le.code@gmail.com>
2020-08-26 23:10:23 -0400

    code cleansing

--- src/objectclassfunction/basic-object-properties.ts
+++ src/objectclassfunction/basic-object-properties.ts
@@ -1,36 +1,6 @@
 
 // since es5 (2009), javascript supports class getter and setter
 
-// define in object literal
-let foo = {
-    name: 'foo',
-    _age: -100,
-    get age() {
-        return this._age * -1;
-    },
-    // age can not be changed, thus no setter
-}
-console.log("foo's age: " + foo.age);
-
-// add getter setter to an existing object
-Object.defineProperty(foo, 'label', {
-    get() {
-        return `our greatest ${this.name}`;
-    },
-    set(lbl: string) {
-        if (lbl) {
-            this.name = lbl.toUpperCase();
-        }
-    }
-})
-// setter supports '=' assignment operator
-foo.label = 'bar';
-console.log("foo's label: " + foo.label); // => our greatest BAR
-
-// BUT, you can still access _age property defined in literal, because javascript
-// does not support private or protected class members
-console.log("foo's secrete age: " + foo._age);
-
 // Typescript supports 'private' keyword in class property definition, 
 // but that support is only offering tsc compiling error check.
 
